771c96f1e6132620e2c8ff2ee5832bcdb4431d7d
mohamedosama2074 <iosengks74@gmail.com>
2019-08-18 02:45:36 +0200

    UIAlertControll&AlertAndActionSheet

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -16,5 +16,62 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func AlertAction(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "my title", message: "my message", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            
+            textField.placeholder = "MY PLACEHOLDER HERE"
+            textField.keyboardType = .numberPad
+        }
+        
+        let alertAction = UIAlertAction(title: "OK", style: .default) { (myAction) in
+            
+            let text = alert.textFields?.first?.text
+            
+            print(text)
+        }
+        let alertAction2 = UIAlertAction(title: "Cancel", style: .cancel) { (myAction) in
+            
+            print("Cancel")
+        }
+        
+        
+        alert.addAction(alertAction)
+        alert.addAction(alertAction2)
+        self.present(alert , animated: true , completion: nil)
+        
+        
+    }
+
+
+
+    @IBAction func ActionSheet(_ sender: UIButton) {
+    
+        let sheet = UIAlertController(title: nil , message: nil, preferredStyle: .actionSheet)
+        
+        let actionSheet = UIAlertAction(title: "Browse", style: .default) { (mysheet) in
+            
+            print("sheet")
+        }
+        let actionSheet1 = UIAlertAction(title: "Browse", style: .default) { (mysheet) in
+            
+            print("sheet1")
+        }
+       
+        let actionSheet3 = UIAlertAction(title: "Cancel", style: .cancel) { (mysheet) in
+            
+            print("cancel")
+        }
+        
+        sheet.addAction(actionSheet)
+        sheet.addAction(actionSheet1)
+        sheet.addAction(actionSheet3)
+        
+        self.present(sheet , animated: true , completion: nil)
+    
+    }
+    
 }
 
